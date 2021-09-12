ce36191508e4158886cde234bb4628d3bc907195
BryanRi <BryanRi@users.noreply.github.com>
2021-09-12 19:34:32 +0200

    enable sloppy focus

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -675,9 +675,9 @@ client.connect_signal("request::titlebars", function(c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
--- client.connect_signal("mouse::enter", function(c)
---    c:emit_signal("request::activate", "mouse_enter", {raise = false})
--- end)
+client.connect_signal("mouse::enter", function(c)
+    c:emit_signal("request::activate", "mouse_enter", {raise = false})
+end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
@@ -690,7 +690,7 @@ autorunApps =
 {
    "compton",
    "fehbg",
---   "redshift -c ~/.config/redshift.conf", --turns on nightlight
+   "redshift -c ~/.config/redshift.conf", --turns on nightlight
    "disable-xdg-screensaver", -- disable screensaver of the root window, find root window with: $ xwininfo -root
 }
 if autorun then
