6b8e927f482769eb83ea6174b0f9499934a9ca62
Bryan Rinders <46681553+BryanRi@users.noreply.github.com>
2022-08-13 09:33:44 +0200

    update

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,9 +1,20 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
+require('plugins/filetype')
 
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
 	vis:command('set theme gruvbox')
+--    --Global configuration--
+    
+--    vis:command('set change-256colors off') --see vis#613
+--    vis:command('set theme base16-tomorrow-night')
+--
+--    --keyboard shortcuts--
+--    vis:command('map insert <C-s> <Escape>:w<Enter>')
+--    vis:command('map normal <C-s> :w<Enter>')
+--    vis:command('map! insert <M-C-h> <Escape>dbi')
+
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
@@ -14,6 +25,5 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	vis:command('set autoindent on')
 	vis:command('set tabwidth 4')
 	vis:command('set expandtab on')
-
 
 end)
